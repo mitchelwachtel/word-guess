998ed3e5eea0cfd8751cc01172f18272c9c25565
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-01-16 17:11:53 -0500

    javascript is functional. Bugs exist

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var buttonEl = document.getElementById("start");
 var timerEl = document.getElementById("timer");
 var winsEl = document.getElementById("wins");
 var losesEl = document.getElementById("loses");
-var gameSpace = document.querySelector(".game-space");
+
 var wins = 0;
 var loses = 0;
 var secLeft = 10;
@@ -11,32 +11,57 @@ var secLeft = 10;
 buttonEl.addEventListener("click", startGame);
 
 function startGame() {
+  var gameSpace = document.querySelector(".game-space");
   secLeft = 10;
   var specialWord = words[Math.floor(Math.random() * words.length)];
   var wordArray = specialWord.split("");
 
   wordBlanks(wordArray);
-  var gameDisplay = wordBlanks(wordArray);
+  var displayArray = wordBlanks(wordArray);
+  
 
   var timer = setInterval(function () {
     timerEl.textContent = secLeft;
     secLeft--;
     if (secLeft < 0) {
       clearInterval(timer);
-      if (gameDisplay.includes("_")) {
+      if (displayArray.includes("_")) {
         loses += 1;
         losesEl.textContent = "Loses: " + loses;
+      } else {
+          wins += 1;
+          winsEl.textContent = "Wins: " + wins;
       }
     }
-  }, 100);
-}
+  }, 1000);
 
-function wordBlanks(x) {
-  var y = "";
-
-  for (i = 0; i < x.length; i++) {
-    y += "_ ";
+  document.addEventListener('keydown', press);
+  
+  function wordBlanks(x) {
+    var y = '';
+    var displayArray = []; 
+  
+    for (i = 0; i < x.length; i++) {
+      y += "_";
+      displayArray.push('_');
+    }
+    gameSpace.textContent = y;
+    return displayArray;
   }
-  gameSpace.textContent = y;
-  return y;
+
+  function press(event) {
+    if (wordArray.includes(event.key)) {
+        console.log(event.key);
+        for (i = 0; i < wordArray.length; i++) {
+          var a = wordArray.indexOf(event.key, i);
+          displayArray[a] = event.key;
+          gameSpace.textContent = displayArray.join('');
+      }
+    } else {
+        return;
+    }
+  }
 }
+
+
+
